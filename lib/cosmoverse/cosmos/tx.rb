edd34ce36531b47895dcc95ca9eaaeb9c76635b1
Armin Kirchner <post.armin@gmail.com>
2022-10-10 15:51:00 +0900

    Representation for transactions
    Easy to understand representation of transactions.

--- lib/cosmoverse/cosmos/tx.rb
+++ lib/cosmoverse/cosmos/tx.rb
@@ -4,23 +4,58 @@ require "cosmoverse/proto/cosmos/tx/v1beta1/service_services_pb"
 
 module Cosmoverse
   module Cosmos
-    module Tx
+    class Tx
+      attr_reader :block_height, :tx_hash, :gas_wanted, :gas_used
+
       SERVICE = Cosmoverse::Proto::Cosmos::Tx::V1beta1::Service::Service
+
+      TransferEvent = Struct.new("TransferEvent", :recipient, :sender, :amount)
 
       def self.received_txs(address)
         events = ["transfer.recipient='#{address}'"]
 
         request = Cosmoverse::Proto::Cosmos::Tx::V1beta1::Service::GetTxsEventRequest.new(events:)
+        response = Cosmoverse::Cosmos::Client.call(request)
 
-        Cosmoverse::Cosmos::Client.call(request)
+        response.tx_responses.map { |txr| new(txr) }
       end
 
       def self.send_txs(address)
         events = ["message.sender='#{address}'"]
 
         request = Cosmoverse::Proto::Cosmos::Tx::V1beta1::Service::GetTxsEventRequest.new(events:)
+        response = Cosmoverse::Cosmos::Client.call(request)
 
-        Cosmoverse::Cosmos::Client.call(request)
+        response.tx_responses.map { |txr| new(txr) }
+      end
+
+      def initialize(tx_response)
+        @block_height = tx_response.height
+        @tx_hash = tx_response.txhash
+        @gas_wanted = tx_response.gas_wanted
+        @gas_used = tx_response.gas_used
+        @logs = tx_response.logs
+      end
+
+      def ==(other)
+        tx_hash == other.tx_hash
+      end
+
+      alias eql? ==
+
+      def hash
+        [self.class, tx_hash].hash
+      end
+
+      def events
+        @logs.map do |log|
+          case log.events.map(&:type)
+          when %w[coin_received coin_spent message transfer]
+            TransferEvent.new(*log.events.last.attributes.map(&:value))
+          else
+            raise "Unknown ABCIMessageLog at '#{log.msg_index}' for tx: '#{tx_hash}'"
+          end
+        end
       end
     end
   end
--- test/cosmoverse/cosmos/test_tx.rb
+++ test/cosmoverse/cosmos/test_tx.rb
@@ -6,35 +6,47 @@ module Cosmoverse
   module Cosmos
     class TestTx < Minitest::Test
       def test_received_txs
+        amount = "123uatom"
         to_wallet = GaiaTestHelper.generate_wallet
         from_wallet = GaiaTestHelper.genesis_wallet
-        GaiaTestHelper.send_tokens(to_wallet:, amount: "123uatom")
+        GaiaTestHelper.send_tokens(to_wallet:, amount:)
         GaiaTestHelper.wait_for_next_block
 
-        received_txs =
+        txs =
           Cosmoverse::Cosmos::Tx.received_txs(to_wallet.address)
 
-        tx = received_txs.txs.first
+        assert_equal(1, txs.size)
 
-        assert_kind_of(Cosmoverse::Proto::Cosmos::Tx::V1beta1::Tx::Tx, tx)
-        assert_match(from_wallet.address, tx.body.messages.first.value)
+        event = Cosmoverse::Cosmos::Tx::TransferEvent.new(
+          to_wallet.address,
+          from_wallet.address,
+          amount
+        )
+
+        assert_equal txs.first.events, [event]
       end
 
       def test_send_txs
+        amount = "123uatom"
         to_wallet = GaiaTestHelper.generate_wallet
         from_wallet = GaiaTestHelper.generate_wallet
         GaiaTestHelper.send_tokens(to_wallet: from_wallet, amount: "1000uatom")
         GaiaTestHelper.wait_for_next_block
-        GaiaTestHelper.send_tokens(to_wallet:, from_wallet:, amount: "123uatom")
+        GaiaTestHelper.send_tokens(to_wallet:, from_wallet:, amount:)
         GaiaTestHelper.wait_for_next_block
 
-        send_txs =
+        txs =
           Cosmoverse::Cosmos::Tx.send_txs(from_wallet.address)
 
-        tx = send_txs.txs.last
+        assert_equal(1, txs.size)
 
-        assert_kind_of(Cosmoverse::Proto::Cosmos::Tx::V1beta1::Tx::Tx, tx)
-        assert_match(to_wallet.address, tx.body.messages.first.value)
+        event = Cosmoverse::Cosmos::Tx::TransferEvent.new(
+          to_wallet.address,
+          from_wallet.address,
+          amount
+        )
+
+        assert_equal txs.first.events, [event]
       end
     end
   end
