4178945bd65c937c033d518675dba36431f67254
Armin Kirchner <post.armin@gmail.com>
2022-10-23 13:52:52 +0900

    Recorded API test responses disabled on CI.
    The recorded API responses increase the test performance significantly.
    The recordings are disabled on CI to detect errors due to API changes.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,8 +14,9 @@ require_relative "support/gaia_test_helper"
 VCR.configure do |config|
   config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :webmock
-  config.default_cassette_options = { re_record_interval: 518_400 }
   config.allow_http_connections_when_no_cassette = true
 end
 
-WebMock.disable_net_connect!(allow_localhost: true)
+VCR.turn_off!(ignore_cassettes: true) if ENV["CI"]
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: "rpc.cosmos.network")
