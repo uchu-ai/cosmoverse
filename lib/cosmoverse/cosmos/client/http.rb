e0e6ce0c8aafe6f890f158b16f918500413c18c9
Armin Kirchner <post.armin@gmail.com>
2022-12-11 14:22:44 +0900

    JSON RPC client submits requests with POST

--- lib/cosmoverse/cosmos/client/http.rb
+++ lib/cosmoverse/cosmos/client/http.rb
@@ -5,39 +5,31 @@ module Cosmoverse
     module Client
       class Http < Base
         def call
-          response = Net::HTTP.get_response(uri)
-
-          raise "Request was not successful #{response}" unless response.is_a?(Net::HTTPSuccess)
-
+          response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: ssl?) { |http| http.request(http_request) }
           rpc_response = JSON.parse(response.body)
-
-          raise "RPC response was not successful #{rpc_response}" unless rpc_response.key?("result")
-
           value = Base64.decode64(rpc_response.fetch("result").fetch("response").fetch("value"))
           response_class.decode(value)
         end
 
         private
 
-        def uri
-          URI("#{Cosmoverse::Cosmos.config.tendermint_host}/abci_query").tap do |base_uri|
-            base_uri.query = URI.encode_www_form(params)
-          end
-        end
+        def ssl? = uri.scheme == "https"
 
-        def http_proto = request.to_proto.gsub("\n", '\n')
+        def uri = URI(Cosmoverse::Cosmos.config.tendermint_host)
 
         def path = "/#{service_class.service_name}/#{capitalized_request_method}"
 
         def capitalized_request_method = request_method.to_s.split("_").map(&:capitalize).join
 
-        def params
-          # HACK: Empty proto requests cause errors. Data is not added in that
-          # case.
-          return { path: "\"#{path}\"" } if http_proto == "\\n\x00"
-
-          { path: "\"#{path}\"", data: "\"#{http_proto}\"" }
+        def http_request
+          Net::HTTP::Post.new(uri, "Content-Type" => "application/json").tap do |post|
+            post.body = json_rpc_params.to_json
+          end
         end
+
+        def json_rpc_params = { id: -1, jsonrpc: "2.0", method: "abci_query", params: }
+
+        def params = { path:, data: request.to_proto.unpack1("H*") }
       end
     end
   end
